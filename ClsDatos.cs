8e8cf31d66467172e2c31449c79309fa7a6164d0
yenifer <yenifer.santiago@tecsup.edu.pe>
2020-04-17 00:35:06 -0500

    first commit

---
+++ ClsDatos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfSemana03
+{
+    public class ClsDatos
+    {
+        public SqlConnection LeerCadena()
+        {
+            SqlConnection connection =
+                new SqlConnection("Data Source=sql5047.site4now.net;"+
+                "Initial Catalog=DB_A5A769_yensan;User ID=DB_A5A769_yensan_admin;Password=yenifer12345.;");
+            return connection;
+        }
+
+        public DataTable ListaPedidoFechas(DateTime x, DateTime y)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("Usp_listarPedidosEntreFechas", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@FEC1", x);
+            sqlData.SelectCommand.Parameters.AddWithValue("@FEC2", y);
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+
+        public DataTable ListarDetalle(int x)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("usp_detalle", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@IdPedido", x);
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+
+
+        public double PedidoTotal(Int32 idpedido)
+        {
+            SqlConnection connection = LeerCadena();
+            connection.Open();
+            SqlDataAdapter sqlData = new SqlDataAdapter("usp_total", connection);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@IdPedido", idpedido);
+            sqlData.SelectCommand.Parameters.Add(
+                "@Total", SqlDbType.Money).Direction =
+                ParameterDirection.Output;
+            sqlData.SelectCommand.ExecuteScalar();
+            Int32 total = Convert.ToInt32(
+                    sqlData.SelectCommand.Parameters["@Total"].Value);
+            connection.Close();
+            return total;
+
+        }
+
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSemana03
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClsDatos obj = new ClsDatos();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnConsutar_Click(object sender, RoutedEventArgs e)
+        {
+            dgvPedido.ItemsSource = obj.ListaPedidoFechas(Convert.ToDateTime(txtFechaInicio.Text),
+                Convert.ToDateTime(txtFechaFin.Text)).DefaultView;
+        }
+
+        private void DgvPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idPedido;
+            var item = dgvPedido.SelectedItem as DataRowView;
+            if (null == item) return;
+            idPedido = Convert.ToInt32(item.Row["IdPedido"]);
+            dvgDetallePedido.ItemsSource = obj.ListarDetalle(idPedido).DefaultView;
+            txtTotal.Text = Convert.ToString(obj.PedidoTotal(idPedido));
+        }
+
+
+    }
+}
